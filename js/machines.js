31f774dfb3f059798949d0a0ab2771d37b4b4329
Gouttfi <contact@cgibert.fr>
2022-12-01 21:38:22 +0100

    Voir issue #20, correction d'un bug JS où l'affichage des champs ne se modifiait pas selon le type de machine sélectionné

--- js/machines.js
+++ js/machines.js
@@ -10,7 +10,8 @@ if(document.querySelector('#type') !== null)
 //If reading element
 if(urlParams.get("id") !== null)
 {
-    type = document.querySelector('td.fieldname_type:nth-child(2)').innerHTML;
+    //type = document.querySelector('td.fieldname_type:nth-child(2)').innerHTML;
+    type = document.querySelector('#type').value;
 }
 reloadFields();
 
@@ -23,27 +24,57 @@ $(document.body).on("change","#type",function(){
 //Mises en formes conditionnelles ici
 function reloadFields()
 {
+    //Avant d'ajouter les classes hideobject selon la machine choisie, retrait de ces classes dans tous les champs
+    document.querySelector('.field_numero_serie').classList.remove('hideobject');
+    document.querySelector('.field_immatriculation').classList.remove('hideobject');
+    document.querySelector('.field_kilometrage').classList.remove('hideobject');
+    document.querySelector('.field_heures').classList.remove('hideobject');
+    document.querySelector('.field_derniere_revision').classList.remove('hideobject');
+    document.querySelector('.field_ref_filtre_huile_moteur').classList.remove('hideobject');
+    document.querySelector('.field_ref_filtre_huile_hydrau').classList.remove('hideobject');
+    document.querySelector('.field_type_huile_pont').classList.remove('hideobject');
+    document.querySelector('.field_ref_filtre_air').classList.remove('hideobject');
+    document.querySelector('.field_ref_lames').classList.remove('hideobject');
+    document.querySelector('.field_ref_courroie_moteur').classList.remove('hideobject');
+    document.querySelector('.field_ref_plateau_tondeuse').classList.remove('hideobject');
+    document.querySelector('.field_type_bougies').classList.remove('hideobject');
+
     if(type == 0 || type == "Véhicule")
     {
-        document.querySelector('.field_immatriculation').classList.remove('hideobject');
         document.querySelector('.field_numero_serie').classList.add('hideobject');
-        document.querySelector('.field_kilometrage').classList.remove('hideobject');
         document.querySelector('.field_heures').classList.add('hideobject');
-        document.querySelector('.field_derniere_revision').classList.remove('hideobject');
         document.querySelector('.field_ref_lames').classList.add('hideobject');
         document.querySelector('.field_ref_courroie_moteur').classList.add('hideobject');
         document.querySelector('.field_ref_plateau_tondeuse').classList.add('hideobject');
     }
+    else if(type == 2 || type == "Tondeuse")
+    {
+        document.querySelector('.field_immatriculation').classList.add('hideobject');
+        document.querySelector('.field_kilometrage').classList.add('hideobject');
+        document.querySelector('.field_derniere_revision').classList.add('hideobject');
+        document.querySelector('.field_type_bougies').classList.add('hideobject');
+    }
+    else if(type == 7 || type == "Électroportatif")
+    {
+        document.querySelector('.field_immatriculation').classList.add('hideobject');
+        document.querySelector('.field_kilometrage').classList.add('hideobject');
+        document.querySelector('.field_heures').classList.add('hideobject');
+        document.querySelector('.field_derniere_revision').classList.add('hideobject');
+        document.querySelector('.field_ref_filtre_huile_moteur').classList.add('hideobject');
+        document.querySelector('.field_ref_filtre_huile_hydrau').classList.add('hideobject');
+        document.querySelector('.field_type_huile_pont').classList.add('hideobject');
+        document.querySelector('.field_ref_filtre_air').classList.add('hideobject');
+        document.querySelector('.field_ref_lames').classList.add('hideobject');
+        document.querySelector('.field_ref_courroie_moteur').classList.add('hideobject');
+        document.querySelector('.field_ref_plateau_tondeuse').classList.add('hideobject');
+        document.querySelector('.field_type_bougies').classList.add('hideobject');
+    }
     else
     {
         document.querySelector('.field_immatriculation').classList.add('hideobject');
-        document.querySelector('.field_numero_serie').classList.remove('hideobject');
         document.querySelector('.field_kilometrage').classList.add('hideobject');
-        document.querySelector('.field_heures').classList.remove('hideobject');
         document.querySelector('.field_derniere_revision').classList.add('hideobject');
         document.querySelector('.field_type_bougies').classList.add('hideobject');
-        document.querySelector('.field_ref_lames').classList.remove('hideobject');
-        document.querySelector('.field_ref_courroie_moteur').classList.remove('hideobject');
-        document.querySelector('.field_ref_plateau_tondeuse').classList.remove('hideobject');
+        document.querySelector('.field_ref_plateau_tondeuse').classList.add('hideobject');
     }
 }
